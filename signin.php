a5f9309daf4ef66ee7cb02d78c68890e0da36f2b
leehakya <haiyililee@gmail.com>
2026-02-17 20:06:25 -0500

    update

--- signin.php
+++ signin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <title>TalentSync Homepage</title>
-    <link rel="stylesheet" href="assets/auth.css">
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 
 <body>
