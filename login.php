f394781d3c451a222ebfe595c83a6d37f0a23cc1
leehakya <haiyililee@gmail.com>
2026-02-17 19:45:43 -0500

    update login

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>TalentSync Homepage</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/homepage.css">
+</head>
+
+<body>
+
+<div class="auth-container">
+
+    <h2>Login</h2>
+
+    <form action="login.php" method="POST">
+
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" name="email" placeholder="manager@company.com" required>
+        </div>
+
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Enter your password" required>
+        </div>
+
+        <button type="submit" class="btn btn-black">Login</button>
+
+    </form>
+
+    <p>
+        Don't have an account?
+        <a href="signup.php">Sign Up</a>
+    </p>
+
+</div>
+
+</body>
+</html>
