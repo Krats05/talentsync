5ffc21c3bc443de6c13673dfe39d0922238dda81
leehakya <haiyililee@gmail.com>
2026-02-17 19:47:38 -0500

    update

---
+++ includes/navbar.php
@@ -0,0 +1,11 @@
+<header class="navbar">
+    <div class="nav-left">
+        <div class="logo">T</div>
+        <span class="brand">TalentSync</span>
+    </div>
+
+    <div class="nav-right">
+        <a href="#" class="btn btn-white">Login</a>
+        <a href="#" class="btn btn-black">Sign Up</a>
+    </div>
+</header>
