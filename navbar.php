5aacccd39768331158f7ada589bba71a6ee4f28a
leehakya <haiyililee@gmail.com>
2026-02-19 12:07:39 -0500

    update

--- navbar.php
+++ navbar.php
@@ -7,6 +7,6 @@
 
     <div class="nav-right">
     <a href="login.php?role=hiring" class="btn btn-white">Login</a>
-    <a href="signup.php?role=hiring" class="btn btn-black">Sign Up</a>
+    <a href="signin.php?role=hiring" class="btn btn-black">Sign Up</a>
     </div>
 </header>
