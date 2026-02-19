5d9b7f54fb2867dbf2b3b123ade8edff2bd45b7d
Yutong233-tech <yutongj@gwmail.gwu.edu>
2026-02-18 21:40:17 -0500

    Make footer.php & navbar.php
    Move the code about navbar and footer in index.php to navbar.php and footer.php

--- index.php
+++ index.php
@@ -9,17 +9,7 @@
 <body>
 
     <!-- Top Navbar -->
-    <header class="navbar">
-    <div class="nav-left">
-        <div class="logo">T</div>
-        <span class="brand">TalentSync</span>
-    </div>
-
-    <div class="nav-right">
-        <a href="#" class="btn btn-white">Login</a>
-        <a href="#" class="btn btn-black">Sign Up</a>
-    </div>
-    </header>
+    <?php require_once 'navbar.php'; ?>
 
 
     <!-- Product Description -->
@@ -44,51 +34,6 @@
     
 
     <!-- Footer -->
-    <footer class="footer">
-        <div class="footer-container">
-
-            <div class="footer-grid">
-
-                <!-- Brand Column -->
-                <div class="footer-brand">
-                    <div class="footer-logo">
-                        <div class="logo">T</div>
-                        <span class="brand">TalentSync</span>
-                    </div>
-                    <p class="footer-desc">
-                        The complete recruitment management solution for IT companies.
-                    </p>
-                </div>
-
-                <!-- Product Column -->
-                <div class="footer-col">
-                    <h4>Product</h4>
-                    <a href="#">Features</a>
-                    <a href="#">Pricing</a>
-                </div>
-
-                <!-- Team Column -->
-                <div class="footer-col">
-                    <h4>Team</h4>
-                    <a href="#">About</a>
-                    <a href="#">Contact</a>
-                </div>
-
-                <!-- Legal -->
-                <div class="footer-col">
-                    <h4>Legal</h4>
-                    <a href="#">Privacy</a>
-                    <a href="#">Terms</a>
-                    <a href="#">Security</a>
-                </div>
-
-            </div>
-
-            <div class="footer-copyright">
-                © 2026 TalentSync. All rights reserved.
-            </div>
-
-        </div>
-    </footer>
+    <?php require_once 'footer.php'; ?>
 </body>
 </html>
