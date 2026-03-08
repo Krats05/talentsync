33284f08189c26c7de7b0beda68ab409558c5a43
leehakya <haiyililee@gmail.com>
2026-03-08 06:45:04 -0700

    add job detail php

---
+++ job_detail.php
@@ -0,0 +1,18 @@
+<?php
+include 'includes/db.php';
+include 'includes/navbar.php';
+
+$job_id = $_GET['id'];
+
+$sql = "SELECT * FROM jobs WHERE id = $job_id";
+$result = mysqli_query($conn, $sql);
+$job = mysqli_fetch_assoc($result);
+?>
+
+<h2><?php echo $job['title']; ?></h2>
+<p><?php echo $job['description']; ?></p>
+<p>Location: <?php echo $job['location']; ?></p>
+
+<a href="apply_job.php?id=<?php echo $job['id']; ?>">
+    <button>Apply Now</button>
+</a>
