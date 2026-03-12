a7c210f770bc33360745fcaca018555185aabe30
Yutong233-tech <yutongj@gwmail.gwu.edu>
2026-03-11 22:00:25 -0400

    add viewport

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TalentSync Homepage</title>
     <link rel="stylesheet" href="assets/style.css">
     <link rel="stylesheet" href="assets/homepage.css">
